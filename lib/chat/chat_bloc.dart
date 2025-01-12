c0c5b09e2a5ac248c67a30d7b6cc468a07a35cb6
ivan.lyashenko <ivan.lyashenko.it@gmail.com>
2025-01-13 01:17:15 +0300

    improve ui and add chat sessions

--- lib/chat/chat_bloc.dart
+++ lib/chat/chat_bloc.dart
@@ -1,92 +1,140 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'dart:convert';
-import 'package:http/http.dart' as http;
-import 'package:simple_chat_ai/chat/chat_state.dart';
-import 'package:simple_chat_ai/chat/message.dart';
+import 'package:simple_chat_ai/chat/chat_models.dart';
 import 'package:simple_chat_ai/logger.dart';
+import 'package:simple_chat_ai/chat/chat_service.dart';
+
+class ChatState {
+  final Chat chat;
+  final bool isLoading;
+  final String? errorMessage;
+
+  const ChatState({
+    required this.chat,
+    this.isLoading = false,
+    this.errorMessage,
+  });
+
+  ChatState copyWith({
+    Chat? chat,
+    bool? isLoading,
+    String? errorMessage,
+  }) {
+    return ChatState(
+      chat: chat ?? this.chat,
+      isLoading: isLoading ?? this.isLoading,
+      errorMessage: errorMessage ?? this.errorMessage,
+    );
+  }
+}
 
 abstract class ChatEvent {}
 
-class SendMessage extends ChatEvent {
-  final String sender;
-  final String content;
+class AddMessageEvent extends ChatEvent {
+  final String sessionId;
+  final Message message;
 
-  SendMessage({required this.sender, required this.content});
+  AddMessageEvent({
+    required this.sessionId,
+    required this.message,
+  });
 }
 
-class ClearMessages extends ChatEvent {}
+class CreateSessionEvent extends ChatEvent {
+  final String sessionId;
+
+  CreateSessionEvent({required this.sessionId});
+}
+
+class ClearSessionMessagesEvent extends ChatEvent {
+  final String sessionId;
+
+  ClearSessionMessagesEvent({required this.sessionId});
+}
 
 class ChatBloc extends Bloc<ChatEvent, ChatState> {
-  ChatBloc() : super(const ChatState()) {
-    on<SendMessage>((event, emit) async {
-      try {
-        emit(state.copyWith(isLoading: true, errorMessage: null));
-        final List<Message> messages = List.from(state.messages);
+  ChatBloc(Chat initialChat) : super(ChatState(chat: initialChat)) {
+    on<AddMessageEvent>(_onAddMessage);
+    on<CreateSessionEvent>(_onCreateSession);
+  }
 
-        messages.add(Message(
-          sender: event.sender,
-          content: event.content,
-          timestamp: DateTime.now(),
-        ));
+  Future<void> _onAddMessage(
+      AddMessageEvent event, Emitter<ChatState> emit) async {
+    try {
+      emit(state.copyWith(isLoading: true, errorMessage: null));
 
-        emit(state.copyWith(
-          isLoading: true,
-          messages: messages,
-        ));
+      final Messages messages = List.from(
+        state.chat.sessions
+            .firstWhere((session) => session.id == event.sessionId)
+            .messages,
+      );
 
-        final response = await http.post(
-          Uri.parse('http://localhost:11434/api/chat'),
-          // Uri.parse('http://192.168.100.8:11434/api/chat'),
-          // Uri.parse('http://10.0.2.2:11434/api/chat'),
-          body: jsonEncode({
-            'model': 'codeLlama:7b',
-            'messages': [
-              {'role': 'user', 'content': event.content},
-            ],
-          }),
-          headers: {'Content-Type': 'application/json'},
-        );
-
-        if (response.statusCode != 200 || response.body.isEmpty) {
-          final error =
-              'Error occurred while sending message: ${response.body} (${response.statusCode})';
-          logger.warning(error);
-          emit(state.copyWith(isLoading: false, errorMessage: error));
-          return;
+      final updateSessions = state.chat.sessions.map((session) {
+        if (session.id == event.sessionId) {
+          messages.add(Message(
+            sender: event.message.sender,
+            content: event.message.content,
+            timestamp: DateTime.now(),
+          ));
+          return session.copyWith(messages: messages);
         }
+        return session;
+      }).toList();
 
-        final responseLines = response.body.split('\n');
-        final aiResponse = StringBuffer();
+      emit(state.copyWith(
+        chat: state.chat.copyWith(sessions: updateSessions),
+        isLoading: true,
+      ));
 
-        for (var line in responseLines) {
-          if (line.trim().isEmpty) continue;
+      final response = await ChatService.sendMessage(event.message.content);
 
-          final data = jsonDecode(line);
-          aiResponse.write(data['message']['content']);
+      final aiResponse = StringBuffer();
 
-          if (data['done'] == true) {
-            messages.add(Message(
-              sender: data['model'],
-              content: aiResponse.toString().trim(),
-              timestamp: DateTime.now(),
-            ));
-            break;
-          }
+      for (var line in response) {
+        if (line.trim().isEmpty) continue;
+
+        final data = jsonDecode(line);
+        aiResponse.write(data['message']['content']);
+
+        if (data['done'] == true) {
+          final aiMessage = Message(
+            sender: data['model'],
+            content: aiResponse.toString().trim(),
+            timestamp: DateTime.now(),
+          );
+
+          final updatedSessionsWithAI = state.chat.sessions.map((session) {
+            if (session.id == event.sessionId) {
+              final updatedMessages = List<Message>.from(session.messages)
+                ..add(aiMessage);
+              return session.copyWith(messages: updatedMessages);
+            }
+            return session;
+          }).toList();
+
+          emit(state.copyWith(
+            chat: state.chat.copyWith(sessions: updatedSessionsWithAI),
+            isLoading: false,
+          ));
+          break;
         }
-
-        emit(state.copyWith(
-          isLoading: false,
-          messages: messages,
-        ));
-      } catch (e) {
-        final error = 'Error occurred while sending message: $e';
-        logger.severe(error);
-        emit(state.copyWith(isLoading: false, errorMessage: error));
       }
-    });
+    } catch (e) {
+      final error = 'Error occurred while sending message: $e';
+      logger.severe(error);
+      emit(state.copyWith(isLoading: false, errorMessage: error));
+    }
+  }
 
-    on<ClearMessages>((event, emit) {
-      emit(state.copyWith(messages: []));
-    });
+  void _onCreateSession(CreateSessionEvent event, Emitter<ChatState> emit) {
+    final newSession = ChatSession(id: event.sessionId, messages: []);
+
+    if (state.chat.sessions.any((session) => session.id == event.sessionId)) {
+      return;
+    }
+
+    emit(state.copyWith(
+      chat: state.chat.copyWith(sessions: [...state.chat.sessions, newSession]),
+    ));
   }
 }
--- lib/chat/chat_view.dart
+++ lib/chat/chat_view.dart
@@ -2,16 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 import 'package:simple_chat_ai/chat/chat_bloc.dart';
-import 'package:simple_chat_ai/chat/chat_state.dart';
-import 'package:simple_chat_ai/logger.dart';
+import 'package:simple_chat_ai/chat/chat_models.dart';
+import 'package:uuid/uuid.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final initialChat = Chat(
+      id: Uuid().v4().replaceAll('-', '').substring(0, 8),
+      userName: 'Ryan Gosling',
+      aiModel: 'codeLlama:7b',
+      sessions: [
+        ChatSession(
+          id: Uuid().v4().replaceAll('-', '').substring(0, 8),
+          messages: [],
+        ),
+      ],
+    );
+
     return BlocProvider(
-      create: (_) => ChatBloc(),
+      create: (_) => ChatBloc(initialChat),
       child: const ChatView(),
     );
   }
@@ -26,21 +38,19 @@ class ChatView extends StatefulWidget {
 
 class _ChatViewState extends State<ChatView> {
   final TextEditingController _textController = TextEditingController();
-  final String _currentUser = "Ryan Gosling";
+
   final String _title = 'Ollama Chat';
+
+  late final String _currentUser;
+  late final String _currentSessionId;
 
   @override
   void initState() {
     super.initState();
 
-    context.read<ChatBloc>().stream.listen(
-      (messages) {
-        setState(() {});
-      },
-      onError: (error) {
-        logger.warning('Error in chat stream: $error');
-      },
-    );
+    final chat = context.read<ChatBloc>().state.chat;
+    _currentUser = chat.userName;
+    _currentSessionId = chat.sessions.first.id;
   }
 
   @override
@@ -52,91 +62,33 @@ class _ChatViewState extends State<ChatView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(_title),
-        centerTitle: true,
-      ),
-      body: Column(
+      body: Row(
         children: [
-          BlocBuilder<ChatBloc, ChatState>(
-            buildWhen: (previous, current) =>
-                previous.isLoading != current.isLoading,
-            builder: (context, state) {
-              if (state.isLoading) {
-                return const LinearProgressIndicator();
-              }
-              return const SizedBox.shrink();
-            },
-          ),
-          Expanded(
+          SizedBox(
+            width: 200,
             child: BlocBuilder<ChatBloc, ChatState>(
               builder: (context, state) {
-                if (state.messages.isEmpty) {
-                  return const Center(child: Text('No messages yet'));
-                }
-
                 return ListView.builder(
-                  itemCount: state.messages.length,
+                  itemCount: state.chat.sessions.length,
                   itemBuilder: (context, index) {
-                    final message = state.messages[index];
-                    final isCurrentUser = message.sender == _currentUser;
-
-                    return Align(
-                      alignment: isCurrentUser
-                          ? Alignment.centerRight
-                          : Alignment.centerLeft,
-                      child: Container(
-                        margin: const EdgeInsets.symmetric(
-                            vertical: 5, horizontal: 10),
-                        padding: const EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          color: isCurrentUser
-                              ? Colors.blue[100]
-                              : Colors.grey[300],
-                          borderRadius: BorderRadius.only(
-                            topLeft: const Radius.circular(10),
-                            topRight: const Radius.circular(10),
-                            bottomLeft: Radius.circular(isCurrentUser ? 10 : 0),
-                            bottomRight:
-                                Radius.circular(isCurrentUser ? 0 : 10),
-                          ),
+                    final session = state.chat.sessions[index];
+                    return Card(
+                      child: ListTile(
+                        selected: session.id == _currentSessionId,
+                        title: Text(
+                          session.messages.firstOrNull?.content ??
+                              'No messages',
+                          overflow: TextOverflow.ellipsis,
                         ),
-                        child: Column(
-                          crossAxisAlignment: isCurrentUser
-                              ? CrossAxisAlignment.end
-                              : CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              message.sender,
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: isCurrentUser
-                                    ? Colors.blue[800]
-                                    : Colors.grey[800],
-                              ),
-                            ),
-                            const SizedBox(height: 5),
-                            !isCurrentUser && message == state.messages.last
-                                ? StreamBuilder<String>(
-                                    stream:
-                                        _simulateMessageTyping(message.content),
-                                    builder: (context, snapshot) {
-                                      return Text(snapshot.data ?? '',
-                                          style: const TextStyle(fontSize: 16));
-                                    },
-                                  )
-                                : Text(message.content,
-                                    style: const TextStyle(fontSize: 16)),
-                            const SizedBox(height: 5),
-                            Text(
-                              DateFormat('HH:mm').format(message.timestamp),
-                              style: const TextStyle(
-                                fontSize: 10,
-                                color: Colors.grey,
-                              ),
-                            ),
-                          ],
+                        subtitle: Text(
+                          'id: ${session.id}',
+                          overflow: TextOverflow.ellipsis,
                         ),
+                        onTap: () {
+                          setState(() {
+                            _currentSessionId = session.id;
+                          });
+                        },
                       ),
                     );
                   },
@@ -144,36 +96,156 @@ class _ChatViewState extends State<ChatView> {
               },
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
+          const VerticalDivider(thickness: 1, width: 1),
+          Expanded(
+            child: Column(
               children: [
+                AppBar(
+                  title: Text(_title),
+                  centerTitle: true,
+                ),
+                BlocBuilder<ChatBloc, ChatState>(
+                  buildWhen: (previous, current) =>
+                      previous.isLoading != current.isLoading,
+                  builder: (context, state) {
+                    if (state.isLoading) {
+                      return const LinearProgressIndicator();
+                    }
+                    return const SizedBox.shrink();
+                  },
+                ),
                 Expanded(
-                  child: TextField(
-                    controller: _textController,
-                    decoration: InputDecoration(
-                      border: const OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                      ),
-                      hintText: 'Enter your message...',
-                      suffixIcon: IconButton(
-                        onPressed: () {
-                          context.read<ChatBloc>().add(SendMessage(
-                              sender: _currentUser,
-                              content: _textController.text));
-
-                          _textController.clear();
-                        },
-                        icon: const Icon(Icons.send),
-                      ),
-                    ),
+                  child: BlocBuilder<ChatBloc, ChatState>(
+                    buildWhen: (previous, current) =>
+                        previous.chat != current.chat,
+                    builder: (context, state) {
+                      return _buildMessagesView(state);
+                    },
                   ),
                 ),
+                _buildMessageInput(context),
               ],
             ),
           ),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () => context.read<ChatBloc>().add(
+              CreateSessionEvent(
+                sessionId: Uuid().v4().replaceAll('-', '').substring(0, 8),
+              ),
+            ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.miniStartFloat,
+    );
+  }
+
+  Widget _buildMessageInput(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: _textController,
+              decoration: InputDecoration(
+                border: const OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                ),
+                hintText: 'Enter your message...',
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    context.read<ChatBloc>().add(
+                          AddMessageEvent(
+                            sessionId: _currentSessionId,
+                            message: Message(
+                              sender: _currentUser,
+                              content: _textController.text.trim(),
+                              timestamp: DateTime.now(),
+                            ),
+                          ),
+                        );
+
+                    _textController.clear();
+                  },
+                  icon: const Icon(Icons.send),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildMessagesView(ChatState state) {
+    final session = state.chat.sessions.firstWhere(
+      (session) => session.id == _currentSessionId,
+      orElse: () => ChatSession(id: _currentSessionId, messages: []),
+    );
+
+    if (session.messages.isEmpty) {
+      return const Center(child: Text('No messages yet'));
+    }
+
+    return ListView.builder(
+      itemCount: session.messages.length,
+      itemBuilder: (context, index) {
+        final message = session.messages[index];
+        final isCurrentUser = message.sender == _currentUser;
+
+        return Align(
+          alignment:
+              isCurrentUser ? Alignment.centerRight : Alignment.centerLeft,
+          child: Container(
+            margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: isCurrentUser ? Colors.blue[100] : Colors.grey[300],
+              borderRadius: BorderRadius.only(
+                topLeft: const Radius.circular(10),
+                topRight: const Radius.circular(10),
+                bottomLeft: Radius.circular(isCurrentUser ? 10 : 0),
+                bottomRight: Radius.circular(isCurrentUser ? 0 : 10),
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: isCurrentUser
+                  ? CrossAxisAlignment.end
+                  : CrossAxisAlignment.start,
+              children: [
+                Text(
+                  message.sender,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: isCurrentUser ? Colors.blue[800] : Colors.grey[800],
+                  ),
+                ),
+                const SizedBox(height: 5),
+                !isCurrentUser && message == session.messages.last
+                    ? StreamBuilder<String>(
+                        stream: _simulateMessageTyping(message.content),
+                        builder: (context, snapshot) {
+                          return Text(snapshot.data ?? '',
+                              style: const TextStyle(fontSize: 16));
+                        },
+                      )
+                    : Text(message.content,
+                        style: const TextStyle(fontSize: 16)),
+                const SizedBox(height: 5),
+                Text(
+                  DateFormat('HH:mm').format(message.timestamp),
+                  style: const TextStyle(
+                    fontSize: 10,
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 
@@ -183,4 +255,29 @@ class _ChatViewState extends State<ChatView> {
       yield fullMessage.substring(0, i);
     }
   }
+
+  // int _getSelectedSessionIndex(BuildContext context) {
+  //   final sessions = context.read<ChatBloc>().state.chat.sessions;
+  //   return sessions.indexWhere((session) => session.id == _currentSessionId);
+  // }
+
+  // List<NavigationRailDestination> _buildSessionDestinations(
+  //     BuildContext context) {
+  //   final sessions = context.read<ChatBloc>().state.chat.sessions;
+  //   return sessions
+  //       .map((session) => NavigationRailDestination(
+  //             icon: const Icon(Icons.chat),
+  //             selectedIcon: const Icon(Icons.chat_bubble),
+  //             label: Text(session.id),
+  //           ))
+  //       .toList();
+  // }
+
+  // void _addNewSession() {
+  //   final newSessionId = DateTime.now().millisecondsSinceEpoch.toString();
+  //   context.read<ChatBloc>().add(CreateSessionEvent(sessionId: newSessionId));
+  //   setState(() {
+  //     _currentSessionId = newSessionId;
+  //   });
+  // }
 }
